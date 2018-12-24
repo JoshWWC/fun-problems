eb7de7c95464f656a9ed3cbae1b7416aeede1e68
JoshWWC <joshuawcleveland@gmail.com>
2018-12-23 19:21:53 -0800

    fixing comments / general readability

--- p1/main.c
+++ p1/main.c
@@ -7,27 +7,32 @@
 #include <string.h>
 #include <check.h>
 
-int maxMatchingChars(const char[], const char[], char[]);
+int longestSubseq(const char[], const char[], char[]);
 int possibleResult(int, const char[], const char[], char[]);
 
-
-START_TEST (possibleResultTest)
-    {
-        char result[2];
-        possibleResult(0, "aaaa", "aa", result);
-        ck_assert_str_eq(result, "aa");
-        ck_assert_str_eq(result, "aaaaaaa");
-    }
-END_TEST
-
 int main() {
+
+    // test our function
+    char *s1[] = { "", "aaaa", "aa", "ABAZDC", "AGGTAB", "ABBA" };
+    char *s2[] = { "", "aa", "aaaa", "BACBAD", "GXTXAYB", "ABCABA" };
+    char result[100]; // expected: aa, aa, ABAD, GTAB, ABBA
+    for (int i = 0; i < 6; i++) {
+        longestSubseq(s1[i], s2[i], result);
+        printf("%s\n", result);
+    }
+
     return 0;
 }
 
-// returns the longest set of noncontagious matching chars between two strings
-int maxMatchingChars(const char *s1, const char *s2, char *result) {
+// returns the longest sub-sequence of (noncontagious!) matching chars between two strings
+// "aaaa", "aa"        => "aa"
+// "ABAZDC", "BACBAD"  => "ABAD"
+// "AGGTAB", "GXTXAYB" => "GTAB"
+// "ABBA", "ABCABA"    => "ABBA"
+int longestSubseq(const char *s1, const char *s2, char *result) {
     char results[strlen(s1)][strlen(s1)];
 
+    // set final return to be the largest of the possible subseqs
     int max = 0;
     for (int i = 0; i < strlen(s1); i++) {
         possibleResult(i, s1, s2, results[i]);
@@ -45,13 +50,21 @@ int maxMatchingChars(const char *s1, const char *s2, char *result) {
  * if s1[c2] == s2[c3], add s1[c2], increment both
  * if s1[c3] != s2[c4 ... n], restart s2 at last matching char + 1, increment s1
  * if s1[c4] == s2[c4], add s1[c4], increment both
+ *
+ * returns sequence of matching chars from a given index into an char array
  */
 int possibleResult(int index, const char *s1, const char *s2, char *result) {
     int resultIndex = 0;
     int lastMatch = 0;
     int i = index;
+
+    // iterate over all chars in s1, starting from given index
     while (i < strlen(s1)) {
+
+        // starting from the last matching index, find all possible matches in s2 to the given s1 char
         for (int j = lastMatch; j < strlen(s2); j++) {
+
+            // if a match exists, add the char to the result, set last match index, and increment s1 index
             if (s1[i] == s2[j]) {
                 result[resultIndex] = s1[i];
                 resultIndex++;
@@ -62,6 +75,7 @@ int possibleResult(int index, const char *s1, const char *s2, char *result) {
         }
         i++;
     }
+
     result[resultIndex] = '\0';
 
     return 0;
